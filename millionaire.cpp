edacab082aed7e7d64ceda480efb239735a78359
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-05-23 06:02:29 -0500

    Spacing

--- millionaire.cpp
+++ millionaire.cpp
@@ -9,9 +9,9 @@ int main() {
 
 	std::cout << "Question 1\n\n";
 	
-	std::cout << "What Frame does Mr. Game and Watch Up B 'Fire' come out Out of Shield\n\n";
+	std::cout << "What Frame does Mr. Game and Watch Up B 'Fire' come out Out of Shield?\n\n";
 
-	std::cout << "A. Frame 3   B.Frame 6  \n";
+	std::cout << "A. Frame 3   B. Frame 6  \n";
 	std::cout << "C. Frame 5   D. Frame 11\n\n";
 
 	std::cout << "Enter your answer: \n";
